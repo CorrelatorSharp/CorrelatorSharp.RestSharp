4c2e511e41762397391937ba5adfd8b11a7197ba
Jason Dryhurst-Smith <jason@codat.io>
2017-11-09 12:02:09 +0000

    add the default header for a client

---
+++ CorrelatorSharp.RestSharp/IRestClientCorrelationIdHeaderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RestSharp;
+
+namespace CorrelatorSharp.RestSharp
+{
+    public static class IRestClientCorrelationIdHeaderExtensions
+    {
+        /// <summary>
+        /// Add the current CorrelatorSharp.ActivityScope identifier as a default http header (X-Correlator-Id).
+        /// 
+        /// Be aware this will add a default header at the call time, and will not be refreshed at the point of execution of any requests.
+        /// Please take note of the lifetime and scope of the IRestClient instance before using.
+        /// </summary>
+        /// <param name="client">The RestSharp.IRestClient</param>
+        public static void AddCorrelationHeader(this IRestClient client)
+        {
+            client.AddDefaultHeader(Headers.CorrelationId, ActivityScope.Current?.Id ?? Guid.NewGuid().ToString());
+        }
+    }
+}
--- CorrelatorSharp.RestSharp/IRestRequestCorrelationIdHeaderExtensions.cs
+++ CorrelatorSharp.RestSharp/IRestRequestCorrelationIdHeaderExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Linq;
-using CorrelatorSharp;
+using RestSharp;
 
-namespace RestSharp
+namespace CorrelatorSharp.RestSharp
 {
     public static class IRestRequestCorrelationIdHeaderExtensions
     {
+        /// <summary>
+        /// Add the current CorrelatorSharp.ActivityScope identifier as a http header (X-Correlator-Id).
+        /// </summary>
+        /// <param name="request">The RestSharp.RestRequest</param>
         public static void AddCorrelationHeader(this IRestRequest request)
         {
             request.AddHeader(Headers.CorrelationId, ActivityScope.Current?.Id ?? Guid.NewGuid().ToString());
